f890caec5869d322d1917018c1489b5508d91b78
KyeongMin <ekmst1027@gmail.com>
2019-02-16 14:58:17 +0900

    python-manna

---
+++ source/ex01.py
@@ -0,0 +1,6 @@
+# 명령행 매개변수
+# D:\대학원\1학년 2학기\mannaEDU\python
+import sys
+args = sys.argv[1:]
+for i in args:
+    print(i)
---
+++ source/ex02.py
@@ -0,0 +1,7 @@
+import sys
+args = sys.argv[1:]
+for i in args:
+    print(i.upper(), end=' ')
+
+# 명령 프롬프트를 실행한 후 아래 명령어를 실행한다.
+# python ex02.py i love you
